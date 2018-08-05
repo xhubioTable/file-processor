509ba6a8852775334bb556fc60a61902d2dea554
Torsten Link <torstenlink@gmx.ce>
2018-08-05 13:48:17 +0200

    wip

---
+++ src/ParserSpecificationConstants.js
@@ -0,0 +1,15 @@
+import { START_COLUMN } from './ParserConstants'
+
+export const RULE = {
+  PK: 'Primary Key',
+  TYPE: 'Field Type',
+  C1: 'Mandatory',
+  C2: 'Minimum',
+  C3: 'Maximum',
+  C4: 'Email',
+  C5: 'Regular Expression',
+}
+
+export const START_COLUMN_RULE = START_COLUMN + 2
+export const KEY_SEVERITY = 'Severity'
+export const KEY_RULE = 'Rule'
